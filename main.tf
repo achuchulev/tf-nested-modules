27bb5c36be30ae28fb4b07022a3705b676d3e904
Atanas Chuchulev <achuchulev@Atanass-MacBook-Pro.local>
2018-12-10 11:26:10 +0200

    format config

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "aws" {
-  region     = "${var.region}"
+  region = "${var.region}"
 }
 
 resource "aws_eip" "ip" {
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "ami" {
   value = "${lookup(var.amis, var.region)}"
 }
+
 output "ip" {
   value = "${aws_eip.ip.public_ip}"
 }
+
 output "consul_server_asg_name" {
   value = "${module.consul.asg_name_servers}"
 }
